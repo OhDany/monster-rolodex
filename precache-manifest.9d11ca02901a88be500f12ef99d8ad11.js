f42804c7cc30ed920ffb4cc3c12bea43bce387ef
Oh Dany <ohdany@Oh-Dany.local>
2020-09-07 21:37:19 -0500

    Updates

---
+++ precache-manifest.9d11ca02901a88be500f12ef99d8ad11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad1a935392cafe2e325cc68735591252",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "f5a80a24558b9c18101a",
+    "url": "/monster-rolodex/static/css/main.3bbf58fb.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/monster-rolodex/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5a80a24558b9c18101a",
+    "url": "/monster-rolodex/static/js/main.43f262f4.chunk.js"
+  },
+  {
+    "revision": "212570b3a5f291f8b8f3",
+    "url": "/monster-rolodex/static/js/runtime-main.4a41f6c3.js"
+  }
+]);
